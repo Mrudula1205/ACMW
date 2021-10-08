9a247d986f3ff9dc57a5a3cc8c33731eb5c80f45
Mrudula1205 <mrudula12523@gmail.com>
2021-10-08 23:18:21 +0400

    First Session

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView name_tv;
+    private EditText name_text;
+    private Button button;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name_tv = findViewById(R.id.name_tv);
+        name_text = findViewById(R.id.name_text);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello ACM", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
